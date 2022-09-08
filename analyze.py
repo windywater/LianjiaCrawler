43fa4fe5f0b348d73cb0142816b80cabc31847e6
liqin <qin.li@yuweioptics.com>
2022-09-08 16:28:50 +0800

    update

--- analyze.py
+++ analyze.py
@@ -16,26 +16,29 @@ def loadHouses(file):
 
 if __name__ == '__main__':
     file1 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-08-27.json"
-    file2 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-08-28.json"
-    file3 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-08-29.json"
-    file4 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-08-30.json"
-    file5 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-08-31.json"
-    file6 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-09-01.json"
+    file2 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-09-02.json"
+    file6 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\wuhan\\2022-09-05.json"
 
     file7 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\shenzhen\\2022-08-31.json"
     file8 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\shenzhen\\2022-09-01.json"
+    file88 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\shenzhen\\2022-09-07.json"
 
     file9 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\beijing\\2022-08-31.json"
-    file10 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\beijing\\2022-09-01.json"
+    file10 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\beijing\\2022-09-07.json"
 
     file11 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\hangzhou\\2022-08-31.json"
     file12 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\hangzhou\\2022-09-01.json"
+    file13 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\hangzhou\\2022-09-07.json"
+
+    file100 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\shanghai\\2022-09-02.json"
+    file101 = os.path.split(os.path.realpath(__file__))[0] + "\\houses\\shanghai\\2022-09-07.json"
     
+    house_total = 0
     houseMap = {}
-    for file in [file7, file8]:
+    for file in [file100, file101]:
         houses = loadHouses(file)
         date = houses["date"]
-        print("house total", len(houses["houses"]))
+        house_total = len(houses["houses"])
         for house in houses["houses"]:
             url = house["u"]
             title = house["t"]
@@ -62,6 +65,6 @@ if __name__ == '__main__':
             if tps[-1] < aver_tp:
                 dec_count += 1
 
-                
+    print("house_total", house_total)                
     print("change count", change_count)
     print("dec count", dec_count)
